c7a2723f983f8896e69cd6753b96f4c1965ac844
hna778 <tuananh.core@gmail.com>
2021-02-08 10:50:07 +0700

    add function cube

--- feb08.js
+++ feb08.js
@@ -3,3 +3,12 @@ function cube(x) {
 }
 
 console.log(cube(10));
+
+function random(x) {
+    if (x >1) {
+      console.log("very big");
+    } else {
+      console.log("small");}
+}
+
+random(10);
