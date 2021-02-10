34629cb8daf9e17da102fa7ab9bbfcc052d75906
hna778 <tuananh.core@gmail.com>
2021-02-10 21:19:23 +0700

    Answer Feb 08.homework

--- feb08-homework.js
+++ feb08-homework.js
@@ -1,4 +1,20 @@
-// homework
-// if ppl asset > 1mil => very rich
-// if ppl asset > 500 => fairly rich
-//                    => poor
+var asset = 800000;
+console.log("Asset ",asset);
+if (asset >1000000) {
+    console.log("rich");
+} else {
+    console.log("poor");
+}
+
+function Asset(x) {
+    console.log("Asset ",x)
+}
+Asset(700000);
+if (asset >1000000) {
+    console.log("rich");
+} 
+if (asset >500000) {
+    console.log("fair rich");
+}else {
+    console.log("poor");
+}
