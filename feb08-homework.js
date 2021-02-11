126cd56369d0ca1390b899fbe9d4c9112a9f82fa
hna778 <tuananh.core@gmail.com>
2021-02-11 20:25:29 +0700

    Add Exercise 3

--- feb08-homework.js
+++ feb08-homework.js
@@ -1,3 +1,4 @@
+// 1
 var asset = 800000;
 console.log("Asset ",asset);
 if (asset >1000000) {
@@ -6,15 +7,36 @@ if (asset >1000000) {
     console.log("poor");
 }
 
+// 2
 function Asset(x) {
-    console.log("Asset ",x)
-}
-Asset(700000);
-if (asset >1000000) {
-    console.log("rich");
-} 
-if (asset >500000) {
-    console.log("fair rich");
-}else {
-    console.log("poor");
-}
+        if (asset >1000000) {
+            console.log("rich");
+        } 
+        if (asset >500000) {
+            console.log("fair rich");
+        } else {
+            console.log("poor");
+        }
+            console.log("Asset ",x);
+    }
+    Asset(700000);
+
+// 3
+function create_hacker_speak() {
+    var input = "javascript is awesome";
+    var output = "";
+    for (var i = 0; i < input.length; i++) {
+        if (input[i] === "a") {
+            output += 4;
+        } else if (input[i] === "e") {
+            output += 3;
+        } else if (input[i] === "i") {
+            output += 1; 
+        } else if (input[i] === "o") {
+            output += 0;
+        } else { (output += input[i]);
+        }
+            console.log(output);
+                }
+
+create_hacker_speak();
